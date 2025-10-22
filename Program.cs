d2593de17bb93a0f4fe17e9415ed8e7bbe3b4a7e
D2st22 <shabaltasvitali@gmail.com>
2025-10-22 13:40:45 +0200

    Swagger setting v.2

--- Program.cs
+++ Program.cs
@@ -38,15 +38,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "DeBillPay API v1");
     c.RoutePrefix = "swagger"; 
 });
-}
 
 app.UseAuthentication();
 app.UseAuthorization();
